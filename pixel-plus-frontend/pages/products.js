8f0f18e828011c2375314d6bcecd69f0d67c2100
Santiago11234 <89491348+Santiago11234@users.noreply.github.com>
2024-01-15 12:18:55 -0600

    finished

--- pixel-plus-frontend/pages/products.js
+++ pixel-plus-frontend/pages/products.js
@@ -3,9 +3,9 @@
 import React, { useState } from "react";
 import NavBar from "../components/navbar/BarNav";
 import AddToCartModal from "../components/products/AddToCartModal";
-import ProductCard from "../components/products/ProductCard"
+import ProductCard from "../components/products/ProductCard";
 
-import products from '../data/productsData';
+import products from "../data/productsData";
 
 import Image from "next/image";
 import styles from "../styles/Products.module.css";
@@ -15,11 +15,11 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 export default function Products() {
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
-const handleSearchChange = (event) => {
- setSearchTerm(event.target.value);
-};
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   const openModal = (product) => {
     setSelectedProduct(product);
@@ -30,8 +30,6 @@ const handleSearchChange = (event) => {
     setModalOpen(false);
   };
 
-
-
   return (
     <main className="min-h-screen text-white">
       <header className=" text-white">
@@ -40,26 +38,32 @@ const handleSearchChange = (event) => {
           <h1 className="text-4xl font-bold">Great Products.</h1>
           <p className="text-2xl mt-2">Even cheaper prices.</p>
           <div className="mt-4 relative w-3/4 sm:w-1/3 mx-auto">
-          <input
- type="text"
- placeholder="Search products..."
- className="px-4 py-2 w-full border rounded-lg text-gray-800 pl-10 pr-10 "
- onChange={handleSearchChange}
-/>
+            <input
+              type="text"
+              placeholder="Search products..."
+              className="px-4 py-2 w-full border rounded-lg text-gray-800 pl-10 pr-10 "
+              onChange={handleSearchChange}
+            />
             <SearchIcon className="w-5 h-5 absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
           </div>
         </div>
       </header>
 
       <div className="container mx-auto px-6 py-8">
-       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-       {products
- .filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase()))
- .map((product) => (
-   <ProductCard key={product.name} product={product} onAddToCart={openModal} />
- ))}
-       </div>
-     </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+          {products
+            .filter((product) =>
+              product.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((product) => (
+              <ProductCard
+                key={product.name}
+                product={product}
+                onAddToCart={openModal}
+              />
+            ))}
+        </div>
+      </div>
 
       <AddToCartModal
         name={selectedProduct?.name}
